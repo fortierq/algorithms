550ab2a24af5c123d5a56d1488744f98c4e4a3c1
Quentin Fortier <qpfortier@gmail.com>
2021-04-24 18:03:23 +0200

    Add classic bfs with queue

--- graph/traversal/bfs.py
+++ graph/traversal/bfs.py
@@ -1,7 +1,19 @@
+from collections import deque
 
 from graph.graph import Graph
 from graph.tree import RootedTree
 
+
+def bfs(s, g: Graph) -> RootedTree:
+    q = deque([s])
+    t = RootedTree(s)
+    while q:
+        u = q.popleft()
+        for v in g[u]:
+            if v not in t:
+                q.append(v)
+                t.add(u, v)
+    return t
 
 def bfs_2list(s, g: Graph) -> RootedTree:
     cur, next = [s], []
