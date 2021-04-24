abad24e2710127ae471bafc971605cfcdef3f274
Quentin Fortier <qpfortier@gmail.com>
2021-04-24 18:11:24 +0200

    Add edge testing in graphs

--- graph/graph.py
+++ graph/graph.py
@@ -6,5 +6,8 @@ class Graph:
         raise NotImplementedError()
     def add(self, u, v):
         raise NotImplementedError()
+    def edge(self, u, v):
+        raise NotImplementedError()
     def vertices(self):
         raise NotImplementedError()
+    
--- graph/tree.py
+++ graph/tree.py
@@ -5,13 +5,15 @@ from collections import defaultdict
 class RootedTree(Graph):
     def __init__(self, root):
         self._pred = dict()
-        self._next = defaultdict(list)
+        self._next = defaultdict(set)
         self._root = root
     def __getitem__(self, v):
         yield from self._next[v]
     def add(self, v, w):
-        self._next[v].append(w)
+        self._next[v].add(w)
         self._pred[w] = v
+    def edge(self, u, v):
+        return v in self._next[u]
     def __contains__(self, e):
         return e == self._root or e in self._pred
     def vertices(self):
