15c4ec72c366106045976c45078e7296c02de238
Quentin Fortier <qpfortier@gmail.com>
2021-04-24 17:55:00 +0200

    Add graph and tree

---
+++ graph/graph.py
@@ -0,0 +1,10 @@
+
+class Graph:
+    def __getitem__(self, v):
+        raise NotImplementedError()
+    def __contains__(self, v):
+        raise NotImplementedError()
+    def add(self, u, v):
+        raise NotImplementedError()
+    def vertices(self):
+        raise NotImplementedError()
---
+++ graph/tree.py
@@ -0,0 +1,21 @@
+from graph.graph import Graph
+from collections import defaultdict
+
+
+class RootedTree(Graph):
+    def __init__(self, root):
+        self._pred = dict()
+        self._next = defaultdict(list)
+        self._root = root
+    def __getitem__(self, v):
+        yield from self._next[v]
+    def add(self, v, w):
+        self._next[v].append(w)
+        self._pred[w] = v
+    def __contains__(self, e):
+        return e == self._root or e in self._pred
+    def vertices(self):
+        yield self._root
+        yield from self._pred
+    def pred(self, v):
+        return self._pred.get(v, None)
